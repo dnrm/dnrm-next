47ce4a77e1121ad0adf3bd263b236aa046b69bbe
Daniel Medina <daniel@medina.com>
2021-03-22 10:32:46 -0600

    changed url to random from unsplash

--- components/Layout.js
+++ components/Layout.js
@@ -4,18 +4,11 @@ import Link from 'next/link';
 
 export default function Layout({ children }) {
 
-    const images = [
-        'https://cdn.danielmedina.dev/photo1.jpg',
-        'https://cdn.danielmedina.dev/photo2.jpg',
-        'https://cdn.danielmedina.dev/photo3.jpg',
-        'https://cdn.danielmedina.dev/photo4.jpg',
-    ]
-
     return (
         <div className={styles.container}>
             <Link href="/"><a className={styles.nav}>dnrm</a></Link>
             <div className={styles.content}>
-                <img src={images[Math.floor(Math.random() * images.length)]} className={styles.cover} />
+                <img src="https://source.unsplash.com/random?nature" className={styles.cover} />
                 <div className={styles.text}>
                     { children }
                 </div>
