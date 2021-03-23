d978bcc22d589128f5bf7ee967d2b3339ec92a8b
Daniel Medina <daniel@medina.com>
2021-03-23 07:17:14 -0600

    changed url again

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
         <div className={styles.container}>
             <Link href="/"><a className={styles.nav}>dnrm</a></Link>
             <div className={styles.content}>
-                <img src="https://source.unsplash.com/random?nature" className={styles.cover} />
+                <img src={/*`https://source.unsplash.com/random?nature`*/ `/background.jpg`} className={styles.cover} />
                 <div className={styles.text}>
                     { children }
                 </div>
