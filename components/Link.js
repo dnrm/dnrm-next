2ad126f675a58763e380e6924937d12adb34b19e
Daniel Medina <daniel@medina.com>
2021-04-05 14:18:40 -0500

    :apple: Reformatted code

--- components/Link.js
+++ components/Link.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const Link = (props) => {
-    return (
-        <p><a href={props.href} className="link" rel="noopener" target="_blank">{props.children}</a></p>
-    )
-}
+const Link = (props) => (
+  <p>
+    <a href={props.href} className="link" rel="noopener" target="_blank">
+      {props.children}
+    </a>
+  </p>
+);
 
 export default Link;
