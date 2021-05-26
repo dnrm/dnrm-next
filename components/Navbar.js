d5472e3afa4ab2d58fd071581916f34d1471caa8
Daniel Medina <daniel@medina.com>
2021-05-26 13:00:14 -0500

    Developed most of the dashboard page

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,25 +4,27 @@ import Link from "next/link";
 import styles from "../styles/Layout.module.css";
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <Link href="/">
-        <a className={styles.nav}>dnrm</a>
-      </Link>
-      <Link href="/about">
-        <a className={styles.nav}>about</a>
-      </Link>
-      <Link href="/projects">
-        <a className={styles.nav}>projects</a>
-      </Link>
-      <Link href="/contact">
-        <a className={styles.nav}>contact</a>
-      </Link>
-      <Link href="/dashboard">
-        <a className={styles.nav}>dashboard</a>
-      </Link>
-    </nav>
-  );
+    return (
+        <header className={styles.header}>
+            <nav className={styles.navbar}>
+                <Link href="/">
+                    <a className={styles.nav}>dnrm</a>
+                </Link>
+                <Link href="/about">
+                    <a className={styles.nav}>about</a>
+                </Link>
+                <Link href="/projects">
+                    <a className={styles.nav}>projects</a>
+                </Link>
+                <Link href="/contact">
+                    <a className={styles.nav}>contact</a>
+                </Link>
+                <Link href="/dashboard">
+                    <a className={styles.nav}>dashboard</a>
+                </Link>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Layout from "../../components/Layout";
-import Button from '../../components/Button';
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 
 import styles from "../../styles/Markdown.module.css";
+import img from "../../styles/Image.module.css";
+import dashboard from "../../styles/dashboard.module.css";
 
 const Dasshboard = () => {
+    const [image, setImage] = useState("");
+    const [name, setName] = useState("");
+    const [bio, setBio] = useState("");
+
+    const previewImage = (e) => {
+        setImage(URL.createObjectURL(e.target.files[0]));
+    };
+
     return (
         <>
             <Head>
@@ -14,36 +25,61 @@ const Dasshboard = () => {
             <Layout>
                 <h1 className={styles.h1}>Dashboard</h1>
                 <hr />
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Aliquam maximus arcu rutrum lacinia porttitor. Suspendisse
-                    eget sodales nisi. In commodo congue euismod. Donec
-                    molestie, tortor nec euismod aliquam, dolor est scelerisque
-                    justo, ac tristique arcu nisi nec nibh. Donec consectetur,
-                    dui eget vestibulum congue, neque ligula dapibus lectus, nec
-                    luctus diam erat sed elit. Proin lobortis efficitur tortor,
-                    sed consectetur ex sagittis ac. Nunc eget faucibus massa.
-                    Nullam ultricies augue massa, eu molestie purus rhoncus
-                    vitae.
-                </p>
-                <p>
-                    Donec molestie et turpis at hendrerit. Cras ac auctor neque.
-                    Mauris finibus elit porta neque tempus consequat. Etiam ut
-                    augue molestie, porttitor metus sed, consequat justo.
-                    Maecenas scelerisque viverra condimentum. Suspendisse
-                    potenti. Donec luctus at felis a pulvinar. Phasellus eu
-                    luctus lectus. Donec lacinia vel dolor vitae interdum. Sed
-                    aliquam elit non nisl efficitur, vel dapibus arcu tincidunt.
-                    Phasellus libero ex, vehicula vitae ipsum non, consequat
-                    tristique ex. Sed ultrices ligula eu est maximus auctor et
-                    in nunc. Ut bibendum eros in rutrum semper. Morbi elementum
-                    est sed arcu suscipit, at posuere nunc interdum. Donec
-                    pulvinar nunc ex, sed congue ipsum varius eu. In facilisis
-                    orci ut augue tristique blandit.
-                </p>
-                <Button onClick={() => {
-                    alert('a lol')
-                }}>Click Me</Button>
+                <form>
+                    <div className={dashboard.profile}>
+                        <img
+                            src={
+                                image
+                                    ? image
+                                    : "https://images.unsplash.com/photo-1622035536415-74582a05041c"
+                            }
+                            accept="image/*"
+                            className={img.img}
+                        />
+                        <h1 className={dashboard.name}>{name ? name : "Name"}</h1>
+                        <h3 className={dashboard.bio}>{bio ? bio : "Bio"}</h3>
+                    </div>
+                    <div className={dashboard.inputField}>
+                        <p className={dashboard.label}>Add a profile image</p>
+                        <input
+                            type="file"
+                            name="image"
+                            id="file-image"
+                            onChange={previewImage}
+                        />
+                    </div>
+                    <div className={dashboard.inputField}>
+                        <p className={dashboard.label}>Name</p>
+                        <Input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="Enter your name..."
+                            value={name}
+                            className={dashboard.input}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className={dashboard.inputField}>
+                        <p className={dashboard.label}>Bio</p>
+                        <Input
+                            type="text"
+                            id="bio"
+                            name="bio"
+                            placeholder="Enter you bio..."
+                            value={bio}
+                            className={dashboard.input}
+                            onChange={(e) => setBio(e.target.value)}
+                        />
+                    </div>
+                </form>
+                <Button
+                    onClick={() => {
+                        alert("not working yet lol");
+                    }}
+                >
+                    Save
+                </Button>
             </Layout>
         </>
     );
